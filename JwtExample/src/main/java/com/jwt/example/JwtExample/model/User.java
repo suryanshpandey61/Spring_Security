859a763ed8e29bbc4a45e3f047505e091b651441
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-02-22 15:03:42 +0530

    Models-added

---
+++ JwtExample/src/main/java/com/jwt/example/JwtExample/model/User.java
@@ -0,0 +1,7 @@
+package com.jwt.example.JwtExample.model;
+
+public class User {
+    private String userId;
+    private String name;
+    private String email;
+}
--- JwtExample/src/main/java/com/jwt/example/JwtExample/service/UserService.java
+++ JwtExample/src/main/java/com/jwt/example/JwtExample/service/UserService.java
@@ -1,4 +1,15 @@
 package com.jwt.example.JwtExample.service;
 
+import com.jwt.example.JwtExample.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private List<User> getUser(){
+
+    }
+
 }
