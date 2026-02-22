4536fbc92adffcdace55fa0ea68ba28554a1fadc
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-02-22 15:52:54 +0530

    App-config-added

---
+++ JwtExample/src/main/java/com/jwt/example/JwtExample/config/AppConfig.java
@@ -0,0 +1,27 @@
+package com.jwt.example.JwtExample.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1=User.builder().username("anshu1").password(passwordEncoder().encode("anshu11")).roles("admin").build();
+        UserDetails user2=User.builder().username("anshu2").password(passwordEncoder().encode("anshu22")).roles("admin").build();
+        return new InMemoryUserDetailsManager(user1,user2);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
