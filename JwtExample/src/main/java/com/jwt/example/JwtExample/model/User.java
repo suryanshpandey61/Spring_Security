490dc940d87046c5b36ac89af5d15d6b848f7008
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-02-22 15:11:14 +0530

    user-service-added

--- JwtExample/src/main/java/com/jwt/example/JwtExample/model/User.java
+++ JwtExample/src/main/java/com/jwt/example/JwtExample/model/User.java
@@ -1,5 +1,12 @@
 package com.jwt.example.JwtExample.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User {
     private String userId;
     private String name;
--- JwtExample/src/main/java/com/jwt/example/JwtExample/service/UserService.java
+++ JwtExample/src/main/java/com/jwt/example/JwtExample/service/UserService.java
@@ -3,13 +3,19 @@ package com.jwt.example.JwtExample.service;
 import com.jwt.example.JwtExample.model.User;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserService {
 
-    private List<User> getUser(){
+    private List<User> userList = new ArrayList<>();
 
+    public UserService() {
+      userList.add(new User(UUID.randomUUID().toString(),"Anmol Pandey","anmol@gmail.com"));
+      userList.add(new User(UUID.randomUUID().toString(),"Suryansh Pandey","suryansh@gmail.com"));
+      userList.add(new User(UUID.randomUUID().toString(),"Anubhav Tiwari","anubhav@dev.com"));
+      userList.add(new User(UUID.randomUUID().toString(),"Gaurav Mishra","gaurav@yahoo.com"));
     }
-
 }
