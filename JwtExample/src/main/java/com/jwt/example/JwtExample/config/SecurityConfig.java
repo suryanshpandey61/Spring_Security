2c78a19fd9118f179dc21c23b019e6d4d1b669aa
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-02-23 22:54:33 +0530

    security-config-updated-1

--- JwtExample/src/main/java/com/jwt/example/JwtExample/config/SecurityConfig.java
+++ JwtExample/src/main/java/com/jwt/example/JwtExample/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import com.jwt.example.JwtExample.security.JwtAuthenticationEntryPoint;
 import com.jwt.example.JwtExample.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -12,5 +16,24 @@ public class SecurityConfig {
     private JwtAuthenticationFilter filter;
     @Autowired
     private JwtAuthenticationEntryPoint entryPoint;
-    
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+
+        //configuration
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .cors(cors->cors.disable())
+                .authorizeHttpRequests(
+                        auth->auth.requestMatchers("/home/**")
+                        .authenticated()
+                        .requestMatchers("/auth/login").permitAll()
+                )
+                .exceptionHandling(ex->ex.authenticationEntryPoint(entryPoint))
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        ;
+
+        httpSecurity.addFilterBefore(filter,UsernamePasswordAuthenticationFilter.class);
+        
+        return httpSecurity.build();
+    }
+
 }
