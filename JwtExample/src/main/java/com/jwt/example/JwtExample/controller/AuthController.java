f16c7b380d52a5eff7fa258e06165f786f419267
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-02-24 19:24:37 +0530

    autowired-objects

--- JwtExample/src/main/java/com/jwt/example/JwtExample/controller/AuthController.java
+++ JwtExample/src/main/java/com/jwt/example/JwtExample/controller/AuthController.java
@@ -1,9 +1,20 @@
 package com.jwt.example.JwtExample.controller;
 
+import com.jwt.example.JwtExample.security.JwtHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private AuthenticationManager manager;
+    @Autowired
+    private JwtHelper jwtHelper;
 }
