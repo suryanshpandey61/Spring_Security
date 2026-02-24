c0acf3fc44b63aea4889affb968eeaefd754ba94
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-02-24 19:26:02 +0530

    authetication-manager-added

--- JwtExample/src/main/java/com/jwt/example/JwtExample/config/AppConfig.java
+++ JwtExample/src/main/java/com/jwt/example/JwtExample/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.jwt.example.JwtExample.config;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,5 +25,10 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
+        return builder.getAuthenticationManager();
     }
 }
