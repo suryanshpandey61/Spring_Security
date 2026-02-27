b7620e7c5a16c111eeda2ae9da01951cbf834997
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-02-27 22:00:08 +0530

    jwt-helper-updated

--- JwtExample/src/main/java/com/jwt/example/JwtExample/security/JwtHelper.java
+++ JwtExample/src/main/java/com/jwt/example/JwtExample/security/JwtHelper.java
@@ -14,7 +14,7 @@ import java.util.function.Function;
 @Component
 public class JwtHelper {
 
-    //set token expiry 
+    //set token expiry in mili seconds
     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
     //    public static final long JWT_TOKEN_VALIDITY =  60;
