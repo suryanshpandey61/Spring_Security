9fa194f86c87262314f3b628ad45678e9b4c9071
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-03-14 15:16:17 +0530

    create-user

--- JwtExample/src/main/java/com/jwt/example/JwtExample/service/UserService.java
+++ JwtExample/src/main/java/com/jwt/example/JwtExample/service/UserService.java
@@ -1,6 +1,8 @@
 package com.jwt.example.JwtExample.service;
 
 import com.jwt.example.JwtExample.entities.User;
+import com.jwt.example.JwtExample.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,10 +12,17 @@ import java.util.UUID;
 @Service
 public class UserService {
 
-
+    @Autowired
+    private UserRepository userRepository;
 
 
     public List<User> getUserList(){
-        return userList;
+
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user){
+        user.setUserId(UUID.randomUUID().toString());
+        return userRepository.save(user);
     }
 }
